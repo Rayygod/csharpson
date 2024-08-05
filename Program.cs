c0d1587f458c46c2afc5bf68a9de088fa8c4db70
Vito <vitorfonsecaxs@gmail.com>
2024-08-05 14:40:03 -0300

    beeps

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace csharpson;
+﻿using System.Diagnostics;
+
+namespace csharpson;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Preste atenção no som");
-        Console.Beep();
+        Console.WriteLine("Digite um numero entre os intervalos de 1 a 4");
+        int beeps;
+        beeps = int.Parse(Console.ReadLine());
+
+        if (beeps == 1)
+        {
+            Console.Beep(600,800);
+        }
+        if (beeps == 2)
+        {
+            Console.Beep(500,800);
+        }
+        if (beeps == 3)
+        {
+            Console.Beep(400,800);
+        }
+        if (beeps == 4)
+        {
+            Console.Beep(300,800);
+        }
+
     }
 }
