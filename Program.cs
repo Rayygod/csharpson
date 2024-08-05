3acf030594c97761ff4a78cf69fc87fca8cd128a
Vito <vitorfonsecaxs@gmail.com>
2024-08-05 15:01:06 -0300

    " fimzinho "

--- Program.cs
+++ Program.cs
@@ -8,25 +8,36 @@ class Program
     {
         Console.WriteLine("Preste atenção no som");
         Console.WriteLine("Digite um numero entre os intervalos de 1 a 4");
+
+        // Criar uma variavel e coloquei esse int para falar que tipo de variavel ela é ou seja int
         int beeps;
+        // o Parse usei para converter o texto que o cabra mandou ou seja o numero de 1 a 4 em um inteiro e mandar ela para a variavel beeps
         beeps = int.Parse(Console.ReadLine());
 
+        
         if (beeps == 1)
         {
             Console.Beep(600,800);
         }
+
+
         if (beeps == 2)
         {
             Console.Beep(500,800);
         }
+
+
         if (beeps == 3)
         {
             Console.Beep(400,800);
         }
+
+
         if (beeps == 4)
         {
             Console.Beep(300,800);
-        }
 
+
+        }
     }
 }
